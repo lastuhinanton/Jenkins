f30688c0a68779336826fd6d22e117c3fa2899d2
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 19:42:15 +0300

    update changes

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -2,7 +2,6 @@
     This script push the status of the pipeline to the Telegram bot
 """
 
-import os
 import sys
 import requests
 
@@ -10,12 +9,12 @@ def notify_to_bot(token_id, user_id):
     print(token_id, user_id)
 
 if __name__ == "__main__":
-    print(sys.argv[1].split("___"))
-    # TOKEN = ""
-    # CHAT_ID = 
-    # MESSAGE = """
-    #     Hello word!
-    # """
-    # url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE}"
-    # print(requests.get(url).json())
+    info = sys.argv[1].split("___")
+    TOKEN = info[0]
+    CHAT_ID = info[1]
+    MESSAGE = """
+        Hello word!
+    """
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE}"
+    print(requests.get(url).json())
 

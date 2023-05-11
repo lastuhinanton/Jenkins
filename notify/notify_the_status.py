9d9639d3f38add06e542d9f0164c067e26be10db
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 19:06:31 +0300

    check script

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -2,17 +2,12 @@
     This script push the status of the pipeline to the Telegram bot
 """
 
-import jenkins
-from jenkinsapi.jenkins import Jenkins
+import sys
+
 
 def notify_to_bot(token_id, user_id):
     print(token_id, user_id)
 
 if __name__ == "__main__":
-    jenkins_url = 'http://localhost:8080'
-    username = 'admin'
-    password = 'admin'
-    server = Jenkins(jenkins_url, username=username, password=password)
-    credential_id = 'token_id'
-    creds = server.credentials(credential_id)
-    print(creds.secret.getPlainText())
+    print(sys.argv[1:])
+

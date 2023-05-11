f6b231992ad71674dc4b527d26ae8fb8c57ca7b6
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 20:36:12 +0300

    update notify

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -9,12 +9,13 @@ def notify_to_bot(token_id, user_id):
     print(token_id, user_id)
 
 if __name__ == "__main__":
-    info = sys.argv[1].split("___")
-    TOKEN = info[0]
-    CHAT_ID = info[1]
-    MESSAGE = """
-        Hello word!
-    """
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE}"
-    requests.get(url).json()
+    print(sys.argv[1])
+    # info = sys.argv[1].split("___")
+    # TOKEN = info[0]
+    # CHAT_ID = info[1]
+    # MESSAGE = """
+    #     Hello word!
+    # """
+    # url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE}"
+    # requests.get(url).json()
 

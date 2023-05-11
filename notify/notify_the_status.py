a9daddb3d4604538a5a30ebce99f630bd81da78c
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 19:40:49 +0300

    update script

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -10,7 +10,7 @@ def notify_to_bot(token_id, user_id):
     print(token_id, user_id)
 
 if __name__ == "__main__":
-    print(sys.argv[1])
+    print(sys.argv[1].split("___"))
     # TOKEN = ""
     # CHAT_ID = 
     # MESSAGE = """

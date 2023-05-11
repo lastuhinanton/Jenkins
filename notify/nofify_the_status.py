4303bf0bd45153fee09308e2b1c44d0f20e4388a
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 17:48:50 +0300

    init the script for notification

---
+++ notify/nofify_the_status.py
@@ -0,0 +1,12 @@
+"""
+    This script push the status of the pipeline to the Telegram bot
+"""
+
+import sys
+
+def notify_to_bot(token_id, user_id):
+    print(token_id, user_id)
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+

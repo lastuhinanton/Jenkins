497085c97096a873c089edc500068e6b316af923
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 19:08:45 +0300

    update environ

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -3,11 +3,12 @@
 """
 
 import sys
-
+import os
 
 def notify_to_bot(token_id, user_id):
     print(token_id, user_id)
 
 if __name__ == "__main__":
-    print(sys.argv[1:])
+    token = os.environ['TOKEN_ID']
+    print(token)
 

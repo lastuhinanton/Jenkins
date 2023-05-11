391b8eb351fab6c1bbbcd111ba6205576aefd337
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-11 20:50:21 +0300

    update

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -10,7 +10,7 @@ def notify_to_bot(token_id, user_id):
 
 if __name__ == "__main__":
     TOKEN = sys.argv[1][:-10]
-    CHAT_ID = sys.argv[1][:-10]
+    CHAT_ID = sys.argv[1][-10:]
     print(TOKEN, CHAT_ID)
     MESSAGE = """
         Hello word!

7b77675ab56d9f6163d9e8be4c26aebb234ea2b2
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-12 12:57:26 +0300

    update script

--- notify/notify_the_status.py
+++ notify/notify_the_status.py
@@ -5,27 +5,32 @@
 import sys
 import requests
 
-def notify_to_bot(token_id, user_id):
-    print(token_id, user_id)
-
 # TOKEN = sys.argv[1]
 # CHAT_ID = sys.argv[2]
 # RESULT = sys.argv[3]
 # URL = sys.argv[4]
 # NUMBER = sys.argv[5]
 # NAME = sys.argv[6]
-if __name__ == "__main__":
-    TOKEN = "5962502114:AAF4DI2thksqdSMeTe-3RIWesJADtb79Guk"
-    CHAT_ID = "1190955043"
-    RESULT = "SUCCESS"
-    URL = "http://localhost:8080/job/cd_pipe/81/"
-    NUMBER = "77"
-    NAME = "Build"
+
+def notify_status_stage_to_bot(token_id, user_id):
+    TOKEN = arrguments[2]
+    CHAT_ID = arrguments[3]
+    RESULT = arrguments[4]
+    URL = arrguments[5]
+    NUMBER = arrguments[6]
+    NAME = arrguments[7]
     SMILE= "✅" if RESULT == "SUCCESS" else "🚫"
     MESSAGE = f"""
-========= Stage {NAME}  N> {NUMBER}
+========= Stage {NAME}  N=>{NUMBER}
 Result: {SMILE}{RESULT}{SMILE} 
 Link: {URL}
 """
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE}"
     requests.get(url).json()
+
+if __name__ == "__main__":
+    arrguments = sys.argv
+    if arrguments[1] == "STAGE":
+        notify_status_stage_to_bot(arrguments)
+    elif arrguments[1] == "SUMMARY":
+        pass
